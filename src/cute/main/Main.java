5e4dd1fc5a78f732f2e10db4d0885a7e3e749b4a
Hall Chen <robotics.hchen@gmail.com>
2014-07-10 16:15:40 -0700

    1) added all required libraries for tesseract 2) reorganized 3rd-party resources into "external" folder 3) sample code in Main.java proves tesseract functionality

--- src/cute/main/Main.java
+++ src/cute/main/Main.java
@@ -1,10 +1,25 @@
 
 package cute.main;
 
+import java.io.File;
+import net.sourceforge.tess4j.Tesseract1;
+import net.sourceforge.tess4j.TesseractException;
+
 public class Main {
 
     public static void main(String[] args){
-        System.out.printf("%s %s", "hello world", "hello world");
+        File image = new File("external\\tesseract\\eurotext.tif");
+        Tesseract1 tess = new Tesseract1();
+        tess.setDatapath("external\\tesseract\\tessdata");
+        
+        try{
+            System.out.println(image.canRead());
+            String result = tess.doOCR(image);
+            System.out.print(result);
+        }
+        catch(TesseractException e){
+            System.out.print(e.getMessage());
+        }
     }
     
 }
