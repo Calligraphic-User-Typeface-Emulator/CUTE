4a266ea6d18d2d475695054da993167b2272cd3d
Hall Chen <robotics.hchen@gmail.com>
2014-07-10 16:34:23 -0700

    1) put sample test pictures in "external" folder 2) added gsdll64.dll (ghostscript pdf reader 3) commented sample code in Main.java

---
+++ external/tesseract/readme.txt
@@ -0,0 +1,7 @@
+the .dll files inthe "32bit" folder are the original .dll files that came with the tesseract download.
+
+the .dll files currently in the "tesseract" folder are 64bit versions downloaded from https://github.com/charlesw/tesseract/tree/master/src/lib/TesseractOcr/x64
+
+furthemore, "gsdll4.dll" had to be downloaded from GPL Ghostscript so that pdf files could be read http://sourceforge.net/projects/ghostscript/files
+
+but no worries, everything is still under open source licenses :)
--- src/cute/main/Main.java
+++ src/cute/main/Main.java
@@ -4,18 +4,22 @@ package cute.main;
 import java.io.File;
 import net.sourceforge.tess4j.Tesseract1;
 import net.sourceforge.tess4j.TesseractException;
+import org.apache.log4j.BasicConfigurator;
 
 public class Main {
 
     public static void main(String[] args){
-        File image = new File("external\\tesseract\\eurotext.tif");
-        Tesseract1 tess = new Tesseract1();
-        tess.setDatapath("external\\tesseract\\tessdata");
+        Tesseract1 tess = new Tesseract1();                                     //instantiate tesseract object
+        tess.setDatapath("external\\tesseract\\tessdata");                      //set search path for tess4j package
+        
+        File image = new File("external\\tesseract\\samples\\eurotext.png");    //get picture to do ocr on
+        
+        BasicConfigurator.configure();                                          //configure log4j package
         
         try{
-            System.out.println(image.canRead());
-            String result = tess.doOCR(image);
-            System.out.print(result);
+            System.out.println(image.canRead() + "\n");                         //see if the file is valid image
+            String result = tess.doOCR(image);                                  //perform ocr
+            System.out.print(result);                                           //show results
         }
         catch(TesseractException e){
             System.out.print(e.getMessage());
