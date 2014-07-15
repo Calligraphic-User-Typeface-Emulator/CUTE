b868f6d8a8c4484aa7e0db9ff8944ad06b82b969
Forest Hu <foresthu2006@gmail.com>
2014-07-14 19:42:44 -0700

    added basic outlines for textcompiler and imagemanager classes

---
+++ src/cute/documentmanager/TextCompiler.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cute.documentmanager;
+
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author forest
+ */
+
+//contains tools and methods to read/write to text file
+public class TextCompiler {
+
+    BufferedReader reader = null;
+    BufferedWriter writer = null;
+    
+    //reads file, places images of chars needed to print file into arraylist
+    public ArrayList<BufferedImage> processFile(String filePath) {
+        try {
+            reader = new BufferedReader(new FileReader(filePath));
+        } catch (FileNotFoundException e) {
+
+            System.out.println("Oops. File not found.");
+        }
+
+        //check char to image here
+        try {
+            reader.close();
+        } catch (IOException ex) {
+            Logger.getLogger(TextCompiler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
+    }
+    
+    //writes the images into a specified file
+    public void writeFile(String filePath, ArrayList<BufferedImage> images) {
+
+        try {
+            writer = new BufferedWriter(new FileWriter(filePath));
+        } catch (IOException ex) {
+            Logger.getLogger(TextCompiler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        //write to file
+        
+        try {
+            writer.close();
+        } catch (IOException ex) {
+            Logger.getLogger(TextCompiler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
---
+++ src/cute/imagemanager/ImageManager.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cute.imagemanager;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ *
+ * @author forest
+ */
+
+//contains tools and methods to work with images
+public class ImageManager {
+    
+    
+    //remove background of big image
+    public void removeBackground(ArrayList<BufferedImage> images){
+        
+    }
+    
+    //make sure the picture is valid for our program
+    public void validify(ArrayList<BufferedImage> images){
+        
+    }
+    
+    //split images into individual pictures of characters
+    public Map<String, ArrayList<BufferedImage>> splitImages(ArrayList<BufferedImage> images){
+        return null;
+    }
+    
+    //cut the image so that the individual images are printable
+    //(make sure there isnt excess white space)
+    public void trim(Map<String, ArrayList<BufferedImage>> images){
+        
+    }
+    
+    
+}
