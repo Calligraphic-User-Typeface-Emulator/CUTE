1005e77c0192b316667d426652efe99627631d74
Forest Hu <foresthu2006@gmail.com>
2014-07-16 18:53:48 -0700

    added correct writer objects

--- src/cute/documentmanager/TextCompiler.java
+++ src/cute/documentmanager/TextCompiler.java
@@ -8,11 +8,13 @@ package cute.documentmanager;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
-Import java.io.File;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,49 +23,37 @@ import java.util.logging.Logger;
  *
  * @author forest
  */
-
 //contains tools and methods to read/write to text file
 public class TextCompiler {
 
-    BufferedReader reader = null;
-    BufferedWriter writer = null;
-    
+     
+
     //reads file, places images of chars needed to print file into arraylist
     public ArrayList<BufferedImage> processFile(String filePath) {
-        try {
-            reader = new BufferedReader(new FileReader(filePath));
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+
         } catch (FileNotFoundException e) {
 
             System.out.println("Oops. File not found.");
-        }
-
-        //check char to image here
-        try {
-            reader.close();
-        } catch (IOException ex) {
+        } //check char to image here
+        catch (IOException ex) {
             Logger.getLogger(TextCompiler.class.getName()).log(Level.SEVERE, null, ex);
         }
 
         return null;
     }
-    
+
     //writes the images into a specified file
     public void writeFile(String filePath, ArrayList<BufferedImage> images) {
 
-        try {
-            writer = new BufferedWriter(new FileWriter(new File(filePath)));
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File(filePath)))) {
+            
         } catch (IOException ex) {
             Logger.getLogger(TextCompiler.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
+
         //write to file
         
-        try {
-            writer.close();
-        } catch (IOException ex) {
-            Logger.getLogger(TextCompiler.class.getName()).log(Level.SEVERE, null, ex);
-        }
 
     }
 
